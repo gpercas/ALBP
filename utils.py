0ebd9d8b6c4d509d621f592ccdf5af0da4b05c08
gpercas <guillermo.perezcasbas@outlook.com>
2021-10-10 11:13:37 +0200

    Update utils.py
    Added list_intersection function to utils.

--- utils.py
+++ utils.py
@@ -16,6 +16,21 @@ def list_included(list_1, list_2):
 	set_A, set_B = set(list_1), set(list_2)
 	return set_A.issubset(set_B)
 
+def list_intersection(list_1, list_2):
+	"""
+	Evaluate if all the elements of list_1 are inside list_2.
+	Returns True if positive
+
+	>>> list_included([1,2,3], [1,2,3,4,5])
+		True
+	>>> list_included([], [1,2,3,4,5])
+		True
+	>>> list_included([66,1,2], [1,2,3,4,5])
+		False
+	"""
+
+	set_A, set_B = set(list_1), set(list_2)
+	return set_A.intersection(set_B)
 
 
 def recombine_list(list_, idx):
